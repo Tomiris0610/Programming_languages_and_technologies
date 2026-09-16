f470a47d9e12d098db1a0036c3c26a4dd4a95458
Tomiris0610 <t.arzanbek0610@gmail.com>
2026-09-16 14:29:04 +0500

    Add methods to show info and change specialty

--- Lab3/task1.py
+++ Lab3/task1.py
@@ -3,12 +3,15 @@ class Student:
         self.name = name
         self.age = age
         self.specialty = specialty
+        
     def show_info(self):
         print(f"Имя: {self.name}")
         print(f"Возраст: {self.age}")
         print(f"Специальность: {self.specialty}")
+        
     def change_specialty(self, new_specialty):
         self.specialty = new_specialty
+        
 # Создание объекта
 student = Student()
 # Вывод информации
